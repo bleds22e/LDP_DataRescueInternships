e639bbf3af143fec6a07cff26ef1912df6ffdf5b
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2021-02-02 13:36:31 -0600

    make functions script to make file-processing cleaner

---
+++ Projects/UofA_plant_surveys/functions.R
@@ -0,0 +1,134 @@
+# FUNCTIONS #
+
+read_in_txt_file <- function(file_path){
+  
+  ### READ IN FILE ###
+  ## this attempt using a txt file
+  
+  # open a connection to the file we want to read in
+  con <- file(file_path) 
+  open(con)
+  
+  # make a list to put the results into
+  results_list <- list()
+  
+  # start with the first line in the file and cycle through all of them
+  current_line <- 1
+  while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0) {
+    results_list[current_line] <- line
+    current_line <- current_line + 1
+  } 
+  
+  # close the connection to the file
+  close(con)
+  
+  return(results_list)
+  
+}
+
+
+txt_file_to_df <- function(results_list){
+  
+  ### TURN INTO DATAFRAME ###
+  
+  # remove remaining white spaces and make everything uppercase
+  results_list <- lapply(results_list, str_trim, side = "both") %>% 
+    lapply(., str_squish) %>% 
+    lapply(., str_to_upper)
+  
+  # get the rows after the metadata and unlist them so each value gets read 
+  # separately; otherwise, each row is one big long character string
+  split_list <- lapply(results_list[1:length(results_list)], 
+                       str_split, 
+                       pattern = " ")
+  
+  # find first "quad" row and cut out the rest
+  quad_rows <- vector()
+  for (i in 1:length(split_list)){
+    if (split_list[[i]][[1]][1] == 'QUAD') {
+      quad_rows <- c(quad_rows, i)
+    }
+  }
+  
+  split_list <- split_list[min(quad_rows):length(split_list)]
+  
+  # in order to bind the list together as rows, they need to be the same length.
+  for (i in 1:length(split_list)){
+    
+    # get length of row
+    # 31 is assuming only 1 sub-stand in ever surveyed twice
+    # if 31 is not the maximum length, we'll need to edit this code
+    max_length <- 31
+    row_length <- length(split_list[[i]][[1]])
+    
+    # for lists that start with "QUAD," find length of "row" (actually a list)
+    if (row_length > 1){
+      
+      # this code adds NAs to the row to match max_length
+      if (row_length < max_length) {
+        add_NAs <- vector(mode = "character", length = (max_length - row_length)) %>% 
+          na_if("")
+        split_list[[i]][[1]] <- c(split_list[[i]][[1]], add_NAs)
+      }
+      
+      # for lists that are empty, put the appropriate number of NAs in, based on the
+      # length of the corrected "QUAD"  list  
+    } else if (row_length <= 1) {
+      split_list[[i]][[1]][1:max_length] <- NA
+      
+    } else {
+      print("ERROR: value greater than 31 possibly detected")
+      # ideally, would want to print which file once this is written for multiple
+    }
+    
+  }
+  
+  # stitch lists together to act as rows in a dataframe
+  cover_df <- data.frame(matrix(unlist(split_list), 
+                                nrow = length(split_list), 
+                                byrow = T)) %>% 
+    # remove the empty rows
+    janitor::remove_empty("rows") %>% 
+    # make the first row ("QUAD") into the column names
+    janitor::row_to_names(., row_number = 1) %>% 
+    # remove any remaining "QUAD" rows --- filter throws an error, for some reason
+    .[.$QUAD != 'QUAD',]
+  
+  # make tidy 
+  colnames(cover_df) <- make.unique(colnames(cover_df))
+  cover_df_long <- rename(cover_df, "Species" = "QUAD") %>% 
+    pivot_longer(2:ncol(cover_df), names_to = "Quad") %>% 
+    rename("Cover" = "value")
+  
+  return(cover_df_long)
+  
+}
+
+
+filename_to_metadata <- function(file_path){
+  
+  # fxn to read in HONDO file names and pull out metadata (stand, month, year)
+  myfiles <- as.data.frame(list.files(file_path, 
+                                      full.names = FALSE, 
+                                      pattern = "*.txt"))
+  colnames(myfiles) <- c("file_name")
+  
+  myfiles <- myfiles %>% 
+    separate(file_name, c("hondo", "month", NA)) %>% 
+    mutate("month" = str_to_upper(month),
+           "year" = as.numeric(str_sub(hondo, start = -2)),
+           "stand" = str_sub(hondo, start = -3, end = -3)) %>% 
+    mutate("year" = as.character(if_else(year < 50, year + 2000, year +1900))) %>% 
+    mutate("month" = ifelse(month == 'APR', 4, 
+                            ifelse(month == 'MAY', 5,
+                                   ifelse(month == 'JUN', 6,
+                                          ifelse(month == 'JUL', 7,
+                                                 ifelse(month == 'AUG', 8,
+                                                        ifelse(month == 'SEP', 9, 
+                                                               ifelse(month == 'OCT', 10, NA)))))))) %>% 
+    select(-hondo)
+  
+  return(myfiles)
+  
+}
+
--- Projects/UofA_plant_surveys/read_HONDO_files_to_df.R
+++ Projects/UofA_plant_surveys/read_HONDO_files_to_df.R
@@ -7,126 +7,37 @@
 library(groundhog)
 groundhog.library(tidyverse, "2021-01-01")
 groundhog.library(janitor, "2021-01-01")
+source("UofA_plant_surveys/functions.R")
 
-### NOTES ###
+### FILES ###
 
-# Right now, this script does the following:
-# 1. Reads in a text file. We lose the metadata located in the file name.
-# 2. Pulls out the first line of the file and saves it as metadata.
-# 3. Removes extra white-space and makes each list the same length 
-# 4. Binds the lists together to be rows in one dataframe, names the first row
-#     into column names, drops empty rows and any rows that repeat the column names.
-# 5. Makes a list that contains both the cleaned dataframe and the metadata
+## PREP DATA ##
 
-# Ideally:
-# 1. We either convert to text files in R or figure out how to do this with 
-#    whatever files they currently are.
-# 2. Fix some of the hard-coding.
-# 3. Retain the metadata from the file names.
-# 4. Maybe we want to restructure the files once we've read them all in?
-#     a. read in a file and save the metadata from in (site, stand, year, month)
-#     b. pull out metadata row
-#     c. make the cross-tab data as above
-#     d. save all above in 3 big lists then rotate through and make 1 giant df?
-#         - columns for site, stand, year, month, quad, species, value, temp?, 
-#           and a notes column for the metadata row 
-#         - or keep some of the cross-tab part? or one df for each stand? 
+# make df with .txt file paths
+file_path <- "UofA_plant_surveys/Vascular Plant Surveys/Stand 8"
+myfiles <- as.list(list.files(file_path, full.names = TRUE, pattern = "*.txt"))
 
-### READ IN FILE ###
-## this attempt using a txt file
+# make df with metadata pulled from file names
+metadata <- filename_to_metadata(file_path)
 
-# open a connection to the file we want to read in
-con <- file('UofA_plant_surveys/Vascular Plant Surveys/Stand 8/hondo881.aug.txt') 
-open(con)
+## PROCESS FILES ##
 
-# make a list to put the results into
-results_list <- list()
+# empty list for processed dataframes
+cleaned_list <- list()
 
-# start with the first line in the file and cycle through all of them
-current_line <- 1
-while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0) {
-  results_list[current_line] <- line
-  current_line <- current_line + 1
-} 
-
-# close the connection to the file
-close(con)
-
-### TURN INTO DATAFRAME ###
-
-# remove remaining white spaces and make everything uppercase
-results_list <- lapply(results_list, str_trim, side = "both") %>% 
-  lapply(., str_squish) %>% 
-  lapply(., str_to_upper)
-
-# get the rows after the metadata and unlist them so each value gets read 
-# separately; otherwise, each row is one big long character string
-split_list <- lapply(results_list[1:length(results_list)], 
-                     str_split, 
-                     pattern = " ")
- 
-#### NOTE ####
-# when we code this for running through multiple files, we'll want a loop to run
-# through all the files to find the longest row and set max_length to that value
-
-# find first "quad" row and cut out the rest
-quad_rows <- vector()
-for (i in 1:length(split_list)){
-  if (split_list[[i]][[1]][1] == 'QUAD') {
-    quad_rows <- c(quad_rows, i)
-  }
+for (f in 1:length(myfiles)) {
+  
+  # read in file, clean, and turn into long df
+  f2 <- myfiles[[f]]
+  f3 <- read_in_txt_file(f2)
+  f4 <- txt_file_to_df(f3)
+  
+  # add metadata from file name
+  f4$Month <- metadata$month[f]
+  f4$Year <- metadata$year[f]
+  f4$Stand <- metadata$stand[f]
+  
+  # add to a list
+  cleaned_list[f] <- f4
+  
 }
-
-split_list <- split_list[min(quad_rows):length(split_list)]
-  
-# in order to bind the list together as rows, they need to be the same length.
-for (i in 1:length(split_list)){
-    
-  # get length of row
-  # 31 is assuming only 1 sub-stand in ever surveyed twice
-  # if 31 is not the maximum length, we'll need to edit this code
-  max_length <- 31
-  row_length <- length(split_list[[i]][[1]])
-  
-  # for lists that start with "QUAD," find length of "row" (actually a list)
-  if (row_length > 1){
-    
-    # this code adds NAs to the row to match max_length
-    if (row_length < max_length) {
-      add_NAs <- vector(mode = "character", length = (max_length - row_length)) %>% 
-        na_if("")
-      split_list[[i]][[1]] <- c(split_list[[i]][[1]], add_NAs)
-    }
-
-  # for lists that are empty, put the appropriate number of NAs in, based on the
-  # length of the corrected "QUAD"  list  
-  } else if (row_length <= 1) {
-    split_list[[i]][[1]][1:max_length] <- NA
-    
-  } else {
-    print("ERROR: value greater than 31 possibly detected")
-    # ideally, would want to print which file once this is written for multiple
-  }
-    
-}
-  
-# stitch lists together to act as rows in a dataframe
-cover_df <- data.frame(matrix(unlist(split_list), 
-                          nrow = length(split_list), 
-                          byrow = T)) %>% 
-  # remove the empty rows
-  janitor::remove_empty("rows") %>% 
-  # make the first row ("QUAD") into the column names
-  janitor::row_to_names(., row_number = 1) %>% 
-  # remove any remaining "QUAD" rows --- filter throws an error, for some reason
-  .[.$QUAD != 'QUAD',]
-
-# make tidy 
-# I think what is happening here is that the last 5 columns are repeats and would
-# create rows with example the same data?
-colnames(cover_df) <- make.unique(colnames(cover_df))
-cover_df_long <- rename(cover_df, "Species" = "QUAD") %>% 
-  pivot_longer(2:ncol(cover_df), names_to = "Quad")
-
-# add bits of file name info to df
-
